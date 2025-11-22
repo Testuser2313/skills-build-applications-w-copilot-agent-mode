2381e74c63e346674b57f7e656c1841016639e9e
CoderLko <87602720+Testuser2313@users.noreply.github.com>
2025-11-22 10:58:38 +0000

    Refactor populate_db command to use model methods for data creation and improve readability

--- octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -1,42 +1,52 @@
+
 from django.core.management.base import BaseCommand
 from octofit_tracker.models import User, Team, Activity, Workout, Leaderboard
-from django.utils import timezone
+import datetime
 
 class Command(BaseCommand):
     help = 'Populate the octofit_db database with test data'
 
     def handle(self, *args, **options):
         # Clear existing data
-        Leaderboard.objects.all().delete()
-        Activity.objects.all().delete()
-        Workout.objects.all().delete()
-        User.objects.all().delete()
-        Team.objects.all().delete()
+        Leaderboard.objects.delete()
+        Activity.objects.delete()
+        Workout.objects.delete()
+        User.objects.delete()
+        Team.objects.delete()
 
         # Create teams
-        marvel = Team.objects.create(name='Marvel')
-        dc = Team.objects.create(name='DC')
+        marvel = Team(name='Marvel').save()
+        dc = Team(name='DC').save()
+
+        # Fetch teams
+        marvel = Team.objects.get(name='Marvel')
+        dc = Team.objects.get(name='DC')
 
         # Create users
-        tony = User.objects.create(name='Tony Stark', email='tony@marvel.com', team=marvel)
-        steve = User.objects.create(name='Steve Rogers', email='steve@marvel.com', team=marvel)
-        bruce = User.objects.create(name='Bruce Wayne', email='bruce@dc.com', team=dc)
-        clark = User.objects.create(name='Clark Kent', email='clark@dc.com', team=dc)
+        tony = User(name='Tony Stark', email='tony@marvel.com', team=marvel).save()
+        steve = User(name='Steve Rogers', email='steve@marvel.com', team=marvel).save()
+        bruce = User(name='Bruce Wayne', email='bruce@dc.com', team=dc).save()
+        clark = User(name='Clark Kent', email='clark@dc.com', team=dc).save()
+
+        # Fetch users
+        tony = User.objects.get(email='tony@marvel.com')
+        steve = User.objects.get(email='steve@marvel.com')
+        bruce = User.objects.get(email='bruce@dc.com')
+        clark = User.objects.get(email='clark@dc.com')
 
         # Create activities
-        Activity.objects.create(user=tony, type='Running', duration=30, date=timezone.now())
-        Activity.objects.create(user=steve, type='Cycling', duration=45, date=timezone.now())
-        Activity.objects.create(user=bruce, type='Swimming', duration=60, date=timezone.now())
-        Activity.objects.create(user=clark, type='Yoga', duration=20, date=timezone.now())
+        today = datetime.date.today()
+        Activity(user=tony, type='Running', duration=30, date=today).save()
+        Activity(user=steve, type='Cycling', duration=45, date=today).save()
+        Activity(user=bruce, type='Swimming', duration=60, date=today).save()
+        Activity(user=clark, type='Yoga', duration=20, date=today).save()
 
         # Create workouts
-        w1 = Workout.objects.create(name='Super Strength', description='Strength workout for heroes')
-        w2 = Workout.objects.create(name='Flight Training', description='Aerobic workout for flyers')
-        w1.suggested_for.set([tony, steve, bruce, clark])
-        w2.suggested_for.set([clark, steve])
+        w1 = Workout(name='Super Strength', description='Strength workout for heroes', suggested_for=[tony, steve, bruce, clark]).save()
+        w2 = Workout(name='Flight Training', description='Aerobic workout for flyers', suggested_for=[clark, steve]).save()
 
         # Create leaderboard
-        Leaderboard.objects.create(team=marvel, points=100)
-        Leaderboard.objects.create(team=dc, points=90)
+        Leaderboard(team=marvel, points=100).save()
+        Leaderboard(team=dc, points=90).save()
 
         self.stdout.write(self.style.SUCCESS('Test data populated successfully!'))
